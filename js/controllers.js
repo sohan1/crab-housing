ded40345a0aae726efb12d6ddec1131cda5d9e1a
sohan1 <shn619@gmail.com>
2018-02-08 02:22:04 +0530

    home page almost done

--- js/controllers.js
+++ js/controllers.js
@@ -7,14 +7,59 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
         $scope.navigation = NavigationService.getnav();
 
         $scope.mySlides = [
-            '../img/home-banner/3.jpg', 
+            '../img/home-banner/3.jpg',
             // '../img/home-banner/2.jpg',
             // '../img/home-banner/3.jpg',
         ];
 
-                $timeout( function(){
-            $scope.test1 = "Hello World!";
-        }, 5000 );
+        $timeout(function () {
+            $('.ticker1').easyTicker({
+                direction: 'up',
+                easing: 'swing',
+                speed: 'slow',
+                interval: 2000,
+                height: 'auto',
+                visible: 0,
+                mousePause: 1,
+                controls: {
+                    up: '',
+                    down: '',
+                    toggle: '',
+                    playText: 'Play',
+                    stopText: 'Stop'
+                }
+            });
+        }, 1000);
+        $scope.importantLinks = [{
+                name: '56th ANNUAL REPORT',
+                links: 'http://barc-ecs.com/SabhasadBhavishyaNidhiEngkish.pdf'
+            },
+            {
+                name: 'Sabhasad Bhavishya Nidhi',
+                links: 'http://barc-ecs.com/SabhasadBhavishyaNidhiEngkish.pdf'
+            }, {
+                name: 'FD for 3 Years interest rate 8.50% ',
+                links: 'http://barc-ecs.com/SabhasadBhavishyaNidhiEngkish.pdf'
+            }, {
+                name: 'FD for 2 Years interest rate 8.75%',
+                links: 'http://barc-ecs.com/SabhasadBhavishyaNidhiEngkish.pdf'
+            }, {
+                name: 'FD for 1 Year interst rate 9.00%',
+                links: 'http://barc-ecs.com/SabhasadBhavishyaNidhiEngkish.pdf'
+            }, {
+                name: 'Circular20161205.pdf',
+                links: 'http://barc-ecs.com/SabhasadBhavishyaNidhiEngkish.pdf'
+            }, {
+                name: 'TypeofLoan.pdf',
+                links: 'http://barc-ecs.com/SabhasadBhavishyaNidhiEngkish.pdf'
+            }, {
+                name: 'Circular20161205.pdf',
+                links: 'http://barc-ecs.com/SabhasadBhavishyaNidhiEngkish.pdf'
+            }, {
+                name: 'Circular20161205.pdf',
+                links: 'http://barc-ecs.com/SabhasadBhavishyaNidhiEngkish.pdf'
+            }
+        ]
     })
 
     .controller('ContactCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
