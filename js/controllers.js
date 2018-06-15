7874b708747cd93c2d722be2452693493846d373
jagzwohlig <jagruti@wohlig.com>
2018-06-16 01:07:00 +0530

    comit done

--- js/controllers.js
+++ js/controllers.js
@@ -14,33 +14,27 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
         ];
         $scope.importantLinks = [{
                 name: '56th ANNUAL REPORT',
-                links: 'http://barc-ecs.com/SabhasadBhavishyaNidhiEngkish.pdf'
+                links: 'ar.pdf'
             },
             {
                 name: 'Sabhasad Bhavishya Nidhi',
-                links: 'http://barc-ecs.com/SabhasadBhavishyaNidhiEngkish.pdf'
+                links: 'SabhasadBhavishyaNidhiEngkish.pdf'
             }, {
                 name: 'FD for 3 Years interest rate 8.50% ',
-                links: 'http://barc-ecs.com/SabhasadBhavishyaNidhiEngkish.pdf'
+                links: ''
             }, {
                 name: 'FD for 2 Years interest rate 8.75%',
-                links: 'http://barc-ecs.com/SabhasadBhavishyaNidhiEngkish.pdf'
+                links: ''
             }, {
                 name: 'FD for 1 Year interst rate 9.00%',
-                links: 'http://barc-ecs.com/SabhasadBhavishyaNidhiEngkish.pdf'
+                links: ''
             }, {
-                name: 'Circular20161205.pdf',
-                links: 'http://barc-ecs.com/SabhasadBhavishyaNidhiEngkish.pdf'
+                name: 'Revised rate w.e.f.05.12.2016',
+                links: 'Circular20161205.pdf'
             }, {
-                name: 'TypeofLoan.pdf',
-                links: 'http://barc-ecs.com/SabhasadBhavishyaNidhiEngkish.pdf'
-            }, {
-                name: 'Circular20161205.pdf',
-                links: 'http://barc-ecs.com/SabhasadBhavishyaNidhiEngkish.pdf'
-            }, {
-                name: 'Circular20161205.pdf',
-                links: 'http://barc-ecs.com/SabhasadBhavishyaNidhiEngkish.pdf'
-            }
+                name: 'Criteria for loan',
+                links: 'TypeofLoan.pdf'
+            } 
         ]
     })
     .controller('ContactCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
@@ -112,17 +106,55 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
         $scope.navigation = NavigationService.getnav();
 
         $scope.rules = [{
-                date: 'Model Bye-Laws of Employees Cooperative Societies (2013)',
-                englishUrl: 'Model_Bye-Laws_of_Employees_Cooperative_Societies.pdf',
-                marathiUrl: '',
+            date: 'Model Bye-Laws of Employees Cooperative Societies (2013)',
+            englishUrl: 'Model_Bye-Laws_of_Employees_Cooperative_Societies.pdf',
+            marathiUrl: '',
 
-            },
-            {
-                date: 'Proposed Amendments to Bye-Laws (2004-2005)',
-                englishUrl: 'amend2005.pdf',
-                marathiUrl: 'amend2005-m.pdf',
-            }
-        ];
+        },
+        {
+            date: 'Proposed Amendments to Bye-Laws (2004-2005)',
+            englishUrl: 'amend2005.pdf',
+            marathiUrl: 'amend2005-m.pdf',
+        }
+    ];
+    
+    
+    $scope.amendments = [{
+        title: 'Proposed Amendments for the year 2015-16',
+        circulars: [{
+            content: 'C.1.4 Proposed Amendment-1',
+            englishUrl: 'C.1.4%20%20Proposed%20Amendment.pdf',
+            marathiUrl: '',
+        }, {
+            content: 'F.1.10 Speical Loan Proposed Amendment-2',
+            englishUrl: 'F.1.10%20Speical%20Loan%20%20Proposed%20Amendment-2.pdf',
+            marathiUrl: '',
+        }, {
+            content: 'F.1.10. Emergency Loan Proposed Amendment-3',
+            englishUrl: 'F.1.10.%20Emergency%20Loan%20Proposed%20Amendment-3.pdf',
+            marathiUrl: '',
+        }, {
+            content: 'Loan Rule No. 10 Surety libiality Proposed Amendment-4',
+            englishUrl: 'Loan%20Rule%20No.%2010%20Surety%20libiality%20Proposed%20Amendment-4.pdf',
+            marathiUrl: '',
+        }]
+    },{
+        title: 'Proposed Amendments 2006-07',
+        circulars: [{
+            content: '<p>The proposed amendments in Bye-Law No.<br>G. 1.7(2)<br>F. 1.5</p>',
+            englishUrl: 'amend2007.pdf',
+            marathiUrl: 'amend2007m.pdf',
+        }]
+    },{
+        title: 'Proposed Amendments 2004-05',
+        circulars: [{
+            content: 'The proposed amendments in Bye-Law No.<br>F:1.2 (b) <br>F:1.4 (a) <br>H:1.3 <br>I:1.9 </p>',
+            englishUrl: 'amend2005.pdf',
+            marathiUrl: 'amend2005-m.pdf',
+        }]
+    },
+];
+
     })
     .controller('GalleryCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
         $scope.template = TemplateService.changecontent("gallery"); //Use same name of .html file
